100e051ec1d8659f0f472b400a029a4febe0da6b
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-10-04 22:23:35 +0300

    Add project files.

---
+++ cookie-stand-api/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cookie_stand_api
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
